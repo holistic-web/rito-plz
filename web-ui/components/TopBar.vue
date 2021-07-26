15d74b05932538f4f18d2e6e0f0f13e5e411433e
Michael Fitzhavey <michael.fitzhaavey@gmail.com>
2021-07-26 03:45:54 +0100

    chore: scaffold header

--- web-ui/components/TopBar.vue
+++ web-ui/components/TopBar.vue
@@ -1,11 +1,24 @@
 <template>
   <b-navbar class="TopBar" variant="dark">
     <b-navbar-brand href="#" class="TopBar__brand"> RitoPlz </b-navbar-brand>
+
+    <b-nav>
+      <b-nav-item-dropdown text="Account" right>
+        <b-dropdown-item href="#">Profile</b-dropdown-item>
+        <!-- #TODO: add link to profile page -->
+        <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        <!-- #TODO: add signout functionality  -->
+      </b-nav-item-dropdown>
+    </b-nav>
   </b-navbar>
 </template>
 
 <style lang="scss">
 .TopBar {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
   &__brand {
     color: white !important;
   }
