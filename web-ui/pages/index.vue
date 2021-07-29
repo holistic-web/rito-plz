37f241f0a133b48c6deab30028657c13fd23048b
Michael Fitzhavey <michael.fitzhaavey@gmail.com>
2021-07-30 00:31:09 +0100

    fix: css compilation error

--- web-ui/pages/index.vue
+++ web-ui/pages/index.vue
@@ -11,17 +11,10 @@ export default Vue.extend({})
 </script>
 
 <style lang="scss">
-$menuHeight: 54px;
-
 .Home {
   display: flex;
   width: 100%;
-  min-height: calc(100vh - $menuHeight);
   justify-content: center;
   align-items: center;
-
-  // only use this while it's a holding page, this makes it full screen
-  position: absolute;
-  top: $menuHeight;
 }
 </style>
