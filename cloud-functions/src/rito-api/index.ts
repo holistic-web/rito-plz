ed0e0065de7c7c68fc62e3176829b74570c6c034
andrewlew1s <andrew12lewis@gmail.com>
2021-07-29 19:48:51 -0700

    auth middleware

--- cloud-functions/src/rito-api/index.ts
+++ cloud-functions/src/rito-api/index.ts
@@ -1,18 +1,26 @@
-//import libraries
+// import libraries
 import * as express from 'express';
 import * as bodyParser from "body-parser";
+import { validateFirebaseIdToken } from './middlewares/Authorization'
 
-//initialize express server
-const app = express();
-const main = express();
+// initialize express server
+const app = express()
+const main = express()
 
-//add the path to receive request and set json as bodyParser to process the body 
-main.use('/api/v1', app);
-main.use(bodyParser.json());
-main.use(bodyParser.urlencoded({ extended: false }));
+// add the path to receive request and set json as bodyParser to process the body 
+main.use('/api/v1', app)
+main.use(bodyParser.json())
+main.use(bodyParser.urlencoded({ extended: false }))
+main.use(validateFirebaseIdToken)
 
-main.get('/', (req, res) => {
+main.get('/', (req: any, res) => {
+  console.log('req.user: ', req.user)
   res.send('result')
 })
+
+main.put('/account/summonerId', (req, res) => {
+  // const summonerId = req.body.summonerId
+})
+
 
 export default main
