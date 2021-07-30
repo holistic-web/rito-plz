42e866553bc0fb00f10a1ff7893f4e86d92c6152
Michael Fitzhavey <michael.fitzhaavey@gmail.com>
2021-07-30 19:21:15 +0100

    feat: create account controller

--- cloud-functions/src/rito-api/index.ts
+++ cloud-functions/src/rito-api/index.ts
@@ -1,26 +1,20 @@
 // import libraries
-import * as express from 'express';
-import * as bodyParser from "body-parser";
+import * as express from 'express'
+import * as bodyParser from 'body-parser'
 import { validateFirebaseIdToken } from './middlewares/Authorization'
+import routes from './routes'
 
 // initialize express server
 const app = express()
 const main = express()
 
-// add the path to receive request and set json as bodyParser to process the body 
+// add the path to receive request and set json as bodyParser to process the body
 main.use('/api/v1', app)
 main.use(bodyParser.json())
 main.use(bodyParser.urlencoded({ extended: false }))
 main.use(validateFirebaseIdToken)
 
-main.get('/', (req: any, res) => {
-  console.log('req.user: ', req.user)
-  res.send('result')
-})
-
-main.put('/account/summonerId', (req, res) => {
-  // const summonerId = req.body.summonerId
-})
-
+// configure api route controllers
+main.use('/account', routes.AccountController)
 
 export default main
