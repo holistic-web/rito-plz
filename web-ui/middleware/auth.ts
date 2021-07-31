0ec2aa9299180480f25b0de54361f874eddce42d
Michael Fitzhavey <michael.fitzhaavey@gmail.com>
2021-07-31 23:44:14 +0100

    fix: infinite redirect loop for non logged in users

--- web-ui/middleware/auth.ts
+++ web-ui/middleware/auth.ts
@@ -4,9 +4,7 @@ const auth: Middleware = (context) => {
   const isLoggedIn = context.store.getters['account/isLoggedIn']
   if (isLoggedIn) {
     if (context.route.path === '/login') context.redirect('/')
-  } else {
-    context.redirect('/login')
-  }
+  } else if (context.route.path !== '/login') context.redirect('/login')
 }
 
 export default auth
