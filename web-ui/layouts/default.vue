14a74881ac0e0943041b703d0a8926637cf333b5
Michael Fitzhavey <michael.fitzhaavey@gmail.com>
2021-07-31 04:32:10 +0100

    fix: vuex store reference

--- web-ui/layouts/default.vue
+++ web-ui/layouts/default.vue
@@ -13,7 +13,7 @@ import { mapGetters } from 'vuex'
 export default Vue.extend({
   computed: {
     ...mapGetters({
-      isApiKeyValid: 'server/isApiKeyValid',
+      isApiKeyValid: 'admin/isApiKeyValid',
     }),
   },
 })
