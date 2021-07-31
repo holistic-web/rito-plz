3776f08a99688337132e48b1f8c6c3912ac8b5e2
Michael Fitzhavey <michael.fitzhaavey@gmail.com>
2021-07-31 03:44:14 +0100

    fix: cors issues?

--- cloud-functions/src/rito-api/index.ts
+++ cloud-functions/src/rito-api/index.ts
@@ -1,6 +1,7 @@
 // import libraries
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
+import * as cors from 'cors'
 import { validateFirebaseIdToken } from './middlewares/Authorization'
 import { errorHandler } from './middlewares/ErrorHandler'
 import routes from './routes'
@@ -11,6 +12,7 @@ const main = express()
 
 // add the path to receive request and set json as bodyParser to process the body
 
+main.use(cors({ origin: true }))
 main.use('/api/v1', app)
 main.use(bodyParser.json())
 main.use(bodyParser.urlencoded({ extended: false }))
