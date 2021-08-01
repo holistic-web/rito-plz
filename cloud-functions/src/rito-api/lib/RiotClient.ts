39b281b5217ef3bfaf2befd453d065b9a4aa4273
andrewlew1s <andrew12lewis@gmail.com>
2021-07-31 22:34:57 -0700

    api key check

--- cloud-functions/src/rito-api/lib/RiotClient.ts
+++ cloud-functions/src/rito-api/lib/RiotClient.ts
@@ -4,15 +4,18 @@ const admin = require('firebase-admin')
 export default class RiotClient {
   public static PlatformId = PlatformId
 
-  private async getClient() {
-    const apiKeyRef = admin.database().ref('riot-api-key')
-    const snapshot = await apiKeyRef.get()
-    const apiKey = snapshot.val()
-    return new RiotAPI(apiKey)
+  private async getClient(apiKeyOverride: string) {
+    if (!apiKeyOverride) {
+      const apiKeyRef = admin.database().ref('riot-api-key')
+      const snapshot = await apiKeyRef.get()
+      const apiKey = snapshot.val()
+      return new RiotAPI(apiKey)
+    }
+    return new RiotAPI(apiKeyOverride)
   }
 
-  async getSummoner(region: typeof PlatformId, summonerName: string) {
-    const client = await this.getClient()
+  async getSummoner({ region: string, summonerName: string, apiKey: string }) {
+    const client = await this.getClient(apiKey)
     return client.summoner.getBySummonerName({ region, summonerName })
   }
 }
